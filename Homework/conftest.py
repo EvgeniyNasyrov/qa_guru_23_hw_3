bfdc94476c6765900b75bcabdf748519a1abd598
Evgeniy Nasyrov <kinderht4@gmail.com>
2025-11-10 00:40:36 +0300

    HW done

---
+++ Homework/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def browser():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1280,900")
+
+    driver = webdriver.Chrome(options=options)
+    yield driver
+
+    driver.quit()  # Закрываем браузер после теста
--- Homework/hw_test_open_google_github.py
+++ Homework/hw_test_open_google_github.py
@@ -1,29 +1,16 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-@pytest.fixture
-def driver():
-    opts = Options()
-
-    opts.add_argument("--headless=new")
-    opts.add_argument("--window-size=1280,900")
-    driver = webdriver.Chrome(options=opts)
-    yield driver
-
-    driver.quit()
-
-
-def test_google(driver):
-    url = "https://www.google.com/"
-    driver.get(url)
-    assert driver.title == "Google"
-    assert driver.current_url == url
-
-
-def test_github(driver):
-    driver = webdriver.Chrome()
+def test_github_web(browser):
     url = "https://github.com/"
+    browser.get(url)
 
-    assert "GitHub" in driver.title
-    assert driver.current_url == url
+    assert (
+        browser.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+    )
+    assert browser.current_url == url
+
+
+def test_google_web(browser):
+    url = "https://www.google.com/"
+    browser.get(url)
+
+    assert browser.title == "Google"
+    assert browser.current_url == url
