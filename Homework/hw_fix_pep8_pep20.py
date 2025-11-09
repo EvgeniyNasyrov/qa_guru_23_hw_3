e0ac4c38441d758e31d0dc9daa5f800f1d1ac0df
Evgeniy Nasyrov <kinderht4@gmail.com>
2025-11-09 21:43:51 +0300

    Hw_3.5

---
+++ Homework/hw_fix_pep8_pep20.py
@@ -0,0 +1,19 @@
+print("Hello, README!")
+text = (
+    "Lorem Ipsum - это текст-«рыба», часто используемый в печати и вэб-дизайне."
+    "Lorem Ipsum"
+)
+print(text)
+
+
+def add(a, b):
+    return a + b
+
+
+def greet(name):
+    print("Привет," + name)
+
+
+NUMS = [1, 2, 3, 4, 5]
+print("мир")
+print(add(2, 2))
